77887f13852fa5286c1c981f9d2366744ad2061c
carson-loser <carson.loser@gmail.com>
2023-01-07 17:23:42 -0600

    established required post get and delete routes on server file

--- main/server.js
+++ main/server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const path = require('path');
-// requiring routers for routes
-const htmlRoutes = require('./routes/htmlRoutes');
-// const apiRoutes = require('./routes/apiRoutes')
 
+const table = require('./db/db.json')
 // initializing express app
 const app = express();
 
-// establishing port through heroku default or local 3000
-const PORT = process.env.PORT || 3000;
+// establishing port through heroku default or local 3001
+const PORT = process.env.PORT || 3001;
 
 // middleware for handling user input
 app.use(express.json());
@@ -17,9 +15,68 @@ app.use(express.urlencoded({ extended: true }));
 // default for where we serve our static assets
 app.use(express.static('public'));
 
-// 
-// app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  // res.json(table);
+  // res.json(`${req.method} request received`);
+  // console.info(req.rawHeaders);
+  console.info(`${req.method} request received`);
+
+  return res.json(table);
+});
+
+app.get('/api/notes/:title', (req, res) => {
+  // Coerce the specific search term to lowercase
+  const requestedTitle = req.params.title.toLowerCase();
+
+  // Iterate through the terms name to check if it matches `req.params.term`
+  for (let i = 0; i < table.length; i++) {
+    if (requestedTitle === table[i].title.toLowerCase()) {
+      return res.json(table[i]);
+    }
+  }
+
+  // Return a message if the term doesn't exist in our DB
+  return res.json('No match found');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.post('/api/notes', (req, res) => {
+  // res.json(table);
+  // res.json(`${req.method} request received`);
+  // console.info(req.rawHeaders);
+  console.info(`${req.method} request received`);
+
+  let response;
+
+  if (req.body && req.body.title) {
+    response = {
+      status: 'success',
+      data: req.body
+    };
+    res.json(`Review for ${response.data.title} has been added!`)
+  } else {
+    res.json('Request body must at least contain a product name');
+  }
+
+  console.log(req.body);
+});
+
+app.delete('/api/notes', (req, res) => {
+  res.json(`${req.method} request received`);
+  console.info(req.rawHeaders);
+  console.info(`${req.method} request received`);
+})
 
 // active port
 app.listen(PORT, () => console.log(`Listening on PORT: http://localhost:${PORT}`));
