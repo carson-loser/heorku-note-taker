2e6481c9cd69982c9f1b3de7ca7f18afc55c987a
carson-loser <carson.loser@gmail.com>
2023-01-06 22:34:44 -0600

    changed server boiler plate and deleted contents of other file

--- main/server.js
+++ main/server.js
@@ -1,30 +1,18 @@
-// importing express
 const express = require('express');
-// importing path module from node to establish link between two directorys
 const path = require('path');
-
-// variables for requests to get data from html files and api requests
-const htmlRoutes = require('./routes/htmlRoutes');
-const apiRoutes = require('./routes/apiRoutes');
-
-// intitialize app variable
 const app = express();
-// port variable accepting heroku default or 3001
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
-
-// setting default folder to public
-app.use(express.static('public'));
-
-// / and /api for defaults 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
-
-// middleware for handling user inputs
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-
-app.listen(PORT, () => {
-  console.log(`Listening on PORT: http://localhost:${PORT}`)
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../main/public/notes.html'));
 });
+
+
+
+app.listen(PORT, () => console.log(`Listening on PORT: http://localhost:${PORT}`));
+
+
