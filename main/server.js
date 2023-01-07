c69efa6a28a8da5c5c252828d8907a0d430fc45e
carson-loser <carson.loser@gmail.com>
2023-01-07 01:22:43 -0600

    replaced html server routes with router html route file

--- main/server.js
+++ main/server.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const path = require('path');
+// requiring routers for routes
+const htmlRoutes = require('./routes/htmlRoutes');
+// const apiRoutes = require('./routes/apiRoutes')
+
+// initializing express app
 const app = express();
+
+// establishing port through heroku default or local 3000
 const PORT = process.env.PORT || 3000;
 
+// middleware for handling user input
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// default for where we serve our static assets
 app.use(express.static('public'));
 
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../main/public/notes.html'));
-});
+// 
+// app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-
-
+// active port
 app.listen(PORT, () => console.log(`Listening on PORT: http://localhost:${PORT}`));
 
 
